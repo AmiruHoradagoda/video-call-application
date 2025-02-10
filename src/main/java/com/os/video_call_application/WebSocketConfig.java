1d4a2bf88244f101ce4fa460ea3052884f415def
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-02-11 00:09:01 +0530

    add SockJS support for WebSocket connections and improve error handling in room template

--- src/main/java/com/os/video_call_application/WebSocketConfig.java
+++ src/main/java/com/os/video_call_application/WebSocketConfig.java
@@ -11,6 +11,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new SignalingHandler(), "/signal")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
